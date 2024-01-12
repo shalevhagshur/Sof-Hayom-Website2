f8b64ac2cac12709bed2d698bcdba9d8195eef4f
shalevhagshur <shalev.hagshur@gmail.com>
2024-01-13 00:44:48 +0200

    Layout Complete

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,19 +1,18 @@
-// App.tsx
 import React from 'react';
 import NavBar from './components/NavBar/NavBar';
 import Ads from './components/AdsPlacements/Ads';
 import Footer from './components/Footer/Footer';
 import Home from './components/Home/Home';
-import './App.css'; // Make sure to create this CSS file
+import './App.css';
 
 function App() {
   return (
     <div className="app-container">
       <NavBar />
       <div className="main-content">
-        <Ads className="left-ad" />
+        <Ads className="left-ad" />   {/* Left Ads */}
         <Home />
-        <Ads className="right-ad" />
+        <Ads className="right-ad" /> {/* Right Ads */}
       </div>
       <Footer />
     </div>
